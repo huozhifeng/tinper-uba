5d48a9b614377fecd36714b9c5dae5570b9ff75c
kvkens <kvkens@qq.com>
2017-12-20 13:15:35 +0800

    add:增加在线检测uba的版本.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: Kvkens(yueming@yonyou.com)
  * @Date:   2017-5-15 00:00:00
  * @Last Modified by:   Kvkens
- * @Last Modified time: 2017-12-19 22:49:14
+ * @Last Modified time: 2017-12-20 13:13:51
  */
 
 const os = require("os");
@@ -17,23 +17,31 @@ const util = require("./util");
 //强制第一时间检查node版本，低版本不兼容
 util.checkNodeVersion(6);
 
-
-
+//检测输入命令集合参数
 if (commands.length === 0) {
-  //无参数传递 uba
+  //无参数传递 eg. uba
   if (argv.version || argv.v) {
     util.getVersion();
   }
+  //显示帮助
   util.getHelp();
+  //并且检测npm上最新的uba版本提示
+  util.checkUbaNewVersion().then(v => {
+    console.log(chalk.yellow(`The latest version of NPM : v${v.version}`));
+  }).catch(err => {
+    console.log(chalk.red("check uba version fail timeout!"));
+  });
 } else {
-  //当有参数传递 uba server
-  var opts = {
+  //当有参数传递 eg. uba server
+  //获得uba运行时的一些参数用于传递给插件使用
+  let opts = {
     cmd: commands,
     argv: argv,
+    util,
     name: require("../package.json").name
   };
-  //得到插件的完整路径
-  var pluginPath = util.findPluginPath(commands[0]);
+  //检测调用的插件是否存在？不存在给出警告
+  let pluginPath = util.findPluginPath(commands[0]);
   if (pluginPath) {
     if (require(`uba-${commands[0]}`).plugin) {
       require(`uba-${commands[0]}`).plugin(opts);
--- src/util.js
+++ src/util.js
@@ -2,35 +2,36 @@
  * @Author: Kvkens(yueming@yonyou.com)
  * @Date:   2017-12-19 22:49:14
  * @Last Modified by:   Kvkens
- * @Last Modified time: 2017-12-19 22:49:14
+ * @Last Modified time: 2017-12-20 13:13:58
  */
 
 const chalk = require("chalk");
+const api = require('api-npm');
 
 /**
  * 获得uba的帮助信息
  */
 exports.getHelp = () => {
-    console.log();
-    console.log(chalk.green("  Usage: uba <plugin-name>"));
-    console.log();
-    console.log(chalk.green("  Options:"));
-    console.log();
-    console.log(chalk.green("    -h, --help     output usage information"));
-    console.log(chalk.green("    -v, --version  output the version number"));
-    console.log();
+  console.log();
+  console.log(chalk.green("  Usage: uba <plugin-name>"));
+  console.log();
+  console.log(chalk.green("  Options:"));
+  console.log();
+  console.log(chalk.green("    -h, --help     output usage information"));
+  console.log(chalk.green("    -v, --version  output the version number"));
+  console.log();
 }
 
 /**
  * 检测node.js的指定运行版本
  * @param {*} number 版本号A
  */
-exports.checkNodeVersion = (number) => {
-    var currentNodeVersion = process.versions.node;
-    if (currentNodeVersion.split(".")[0] < number) {
-        console.error(chalk.red(`You are running Node ${currentNodeVersion}\nCreate Uba App requires Node 6 or higher. \nPlease update your version of Node.`));
-        process.exit(1);
-    }
+exports.checkNodeVersion = (number = 6) => {
+  var currentNodeVersion = process.versions.node;
+  if (currentNodeVersion.split(".")[0] < number) {
+    console.error(chalk.red(`You are running Node ${currentNodeVersion}\nCreate Uba App requires Node 6 or higher. \nPlease update your version of Node.`));
+    process.exit(1);
+  }
 }
 
 /**
@@ -38,18 +39,18 @@ exports.checkNodeVersion = (number) => {
  * @param {*} command 插件名
  */
 exports.findPluginPath = (command) => {
-    if (command && /^\w+$/.test(command)) {
-        try {
-            return resolve.sync('uba-' + command, {
-                paths: [path.join(__dirname, '..', 'node_modules')]
-            });
-        } catch (e) {
-            console.log('');
-            console.log('  ' + chalk.green(command) + ' command is not installed.');
-            console.log('  You can try to install it by ' + chalk.blue.bold('npm install uba-' + command) + '.');
-            console.log('');
-        }
+  if (command && /^\w+$/.test(command)) {
+    try {
+      return resolve.sync('uba-' + command, {
+        paths: [path.join(__dirname, '..', 'node_modules')]
+      });
+    } catch (e) {
+      console.log('');
+      console.log('  ' + chalk.green(command) + ' command is not installed.');
+      console.log('  You can try to install it by ' + chalk.blue.bold('npm install uba-' + command) + '.');
+      console.log('');
     }
+  }
 }
 
 
@@ -57,6 +58,26 @@ exports.findPluginPath = (command) => {
  * 获得uba版本
  */
 exports.getVersion = () => {
-    console.log(chalk.green(require("../package.json").version));
-    process.exit(0);
+  console.log(chalk.green(require("../package.json").version));
+  process.exit(0);
+}
+
+/**
+ * 检测最新的uba版本
+ */
+exports.checkUbaNewVersion = () => {
+  return new Promise((resolve, reject) => {
+    api.getdetails("uba", data => {
+      if (data) {
+        resolve({
+          "version": data["dist-tags"].latest,
+          "success": true
+        });
+      } else {
+        reject({
+          "success": false
+        });
+      }
+    });
+  });
 }
